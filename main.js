7157334fbefdebbb455b7a4265734ccbc6d5bc90
JuliannaWQY <juliannawqy@gmail.com>
2017-03-10 23:09:44 +0800

    change2

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
 <!DOCTYPE JavaScript>
 
-var list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10];
-var computer = [16, 17, 18, 19, 20, 21, 22]; 
+$('.row').fadeIn(400);
 
-while ()
+var list = array [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10];
+var computer = array1 [16, 17, 18, 19, 20, 21, 22]; 
+
+while (var i = 0, i <= 6) {
 function main() {
+    $('.color').hide();
+
   $('#GoOnButton').on('click', function() {
+    first.$('.color').fadeIn(400);
+    // ???????????
+
       var Cn = $.select(list);
+
       delete list[Cn];
+      $('.number').console.log(Cn);
+
       var sum = sum + Cn;
+      $('.sum').console.log('Sum:' + sum);
+
       if (sum > 21) {
           var A = $.select(compter)
              if(A = 22) {
-                 // even
+                 //even game
+                 alert('It's an even game!');
              } else {
                  // player 1 loses
+                 alert('Congradulations! You win!');
              }
+      i ++ ;
+      }
          
-$('#test').on('click', function() {
+function main() {
+  $('#EndItButton').on('click', function() {
+
+
+/* $('#test').on('click', function() {
     var ran = items.pop();
     alert(ran ? ran : 'No more numbers in array');
 });
@@ -31,6 +51,6 @@ $('#test').on('click', function() {
 
      $('.skillset').hide();
   $('.skillset').fadeIn(1000);
-  
   $('.projects').hide();
-}
+} 
+*/
