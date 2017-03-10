eeda7f8dab2bc99c6d5dd349063b621d782c489e
JuliannaWQY <juliannawqy@gmail.com>
2017-03-10 22:32:00 +0800

    change1

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 <!DOCTYPE JavaScript>
+
+var list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10];
+var computer = [16, 17, 18, 19, 20, 21, 22]; 
+
+while ()
+function main() {
+  $('#GoOnButton').on('click', function() {
+      var Cn = $.select(list);
+      delete list[Cn];
+      var sum = sum + Cn;
+      if (sum > 21) {
+          var A = $.select(compter)
+             if(A = 22) {
+                 // even
+             } else {
+                 // player 1 loses
+             }
+         
+$('#test').on('click', function() {
+    var ran = items.pop();
+    alert(ran ? ran : 'No more numbers in array');
+});
+      }
+		$(this);
+    $(this).next().slideToggle(400);
+    $(this).toggleClass('active');
+    $(this).text('Project Viewed');
+	});
+
+
+     $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  
+  $('.projects').hide();
+}
